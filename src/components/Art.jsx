44a809ce090cfd4195700a9bd62aa904793ad888
Srinjoy <srinjoyd373@gmail.com>
2026-06-03 13:39:45 +0530

    about art details and animation implementation

---
+++ src/components/Art.jsx
@@ -0,0 +1,89 @@
+import { featureLists, goodLists } from "../../constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useMediaQuery } from "react-responsive";
+
+const Art = () => {
+    const isMobile = useMediaQuery({maxWidth: 767});
+
+    useGSAP(() => {
+        const start = isMobile ? 'top 30%' : 'top top';
+
+        const maskedTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: '#art',
+                start,
+                end: 'bottom center',
+                scrub: 1.5,
+                pin: true,
+
+            }
+        })
+
+        maskedTimeline
+        .to('.will-fade', {
+            opacity: 0,
+            stagger: 0.1,
+            ease: 'power1.inOut',
+        })
+        .to('.masked-img', {
+            scale: 1.1,
+            // maskPosition: 'center',
+            '--mask-size': '400%',
+            duration: 1,
+            ease: 'power2.inOut',
+        }, "<")
+        .to('#masked-content', {
+            opacity: 1,
+            duration: 1,
+            ease: 'power1.inOut',
+        }, "-=0.5")
+
+    })
+
+    return(
+        <div id="art">
+            <div className="container mx-auto h-full pt-20">
+                <h2 className="will-fade">The Art</h2>
+
+                <div className="content">
+                    <ul className="space-y-4 will-fade">
+                        {goodLists.map((feature, index) => (
+
+                            <li key={index} className="flex items-center gap-2">
+                                <img src="/images/check.png" alt="check"/>
+                                <p>{feature}</p>
+                            </li>
+                        ))}
+                    </ul>
+
+                    <div className="cocktail-img">
+                        <img src="/images/under-img.jpg" alt="cocktail-img" className="abs-center masked-img size-full object-contain"/>
+                    </div>
+
+                    <ul className="space-y-4 will-fade">
+                        {featureLists.map((feature, index) => (
+
+                            <li key={index} className="flex items-center justify-start gap-2">
+                                <img src="/images/check.png" alt="check"/>
+                                <p className="md:w-fit w-60">{feature}</p>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className="masked-container">
+                    <h2 className="will-fade">Sip-Worthy Perfection</h2>
+
+                    <div id="masked-content">
+                        <h3>Made with Craft, Poured with Passion</h3>
+                        <p>This isn't just a drink, but an Unforgetable Experience crafted just for You.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
+}
+
+export default Art;
